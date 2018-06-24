e9c6fe6e995b75917e4debcad2ccf3d0fee6e3c0
Romain Lanz <romain.lanz@slynova.ch>
2018-06-24 09:38:12 +0200

    feat(view): start working on can tag

---
+++ src/ViewBindings/Can.js
@@ -0,0 +1,55 @@
+'use strict'
+
+/**
+ * adonis-guard
+ *
+ * @license MIT
+ * @copyright Slynova - Romain Lanz <romain.lanz@slynova.ch>
+ */
+
+const { BaseTag } = require('edge.js')
+
+class Can extends BaseTag {
+  /**
+   * The tag name.
+   *
+   * @return {String}
+   */
+  get tagName () {
+    return 'can'
+  }
+
+  /**
+   * Tag is a block tag.
+   *
+   * @return {Boolean}
+   */
+  get isBlock () {
+    return true
+  }
+
+  /**
+   * Compile the template.
+   *
+   * @param  {Object} compiler
+   * @param  {Object} lexer
+   * @param  {Object} buffer
+   * @param  {String} options.body
+   * @param  {Number} options.lineno
+   *
+   * @return {void}
+   */
+  compile (compiler, lexer, buffer, { body, childs, lineno }) {
+    const [ability, resource, user] = this._compileStatement(lexer, body, lineno).toStatement()
+
+    buffer.writeLine(`if (${this.context.guard.allows(ability, resource, user)}) {`)
+    buffer.indent()
+
+    childs.forEach(child => compiler.parseLine(child))
+
+    buffer.dedent()
+    buffer.writeLine('}')
+  }
+}
+
+module.exports = Can
