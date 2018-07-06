c26d29790d3a1bf4425ea611ac890205aed699f8
Romain Lanz <romain.lanz@slynova.ch>
2018-07-06 10:40:03 +0200

    fix(exception): invoke correctly the exception

--- src/Middleware/Can.js
+++ src/Middleware/Can.js
@@ -12,7 +12,7 @@ const { AuthorizationException } = require('../Exceptions')
 class Can {
   async handle ({ auth, guard }, next, [method, ...argument]) {
     if (!guard.can(auth.user).pass(method).for(argument)) {
-      throw new AuthorizationException()
+      throw AuthorizationException.invoke('Unauthorized', 401, 'E_UNAUTHORIZED')
     }
 
     await next()
